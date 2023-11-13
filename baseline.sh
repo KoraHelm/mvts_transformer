efffd6d57d93c3c1d737db57898087e775ebf7d6
KoraHelm <kora_s.helm@web.de>
2023-11-13 15:50:59 +0100

    changed directory for error file

--- baseline.sh
+++ baseline.sh
@@ -6,7 +6,7 @@
 #SBATCH --time 0-8:00:00
 #SBATCH --ntasks=1
 #SBATCH --output=/mnt/qb/work/eickhoff/esx599/recreate_baseline.out
-#SBATCH --error=/mnt/qb/eickhoff/esx599/recreate_baseline.err
+#SBATCH --error=/mnt/qb/work/eickhoff/esx599/recreate_baseline.err
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=kora.helm@student.uni-tuebingen.de
 
