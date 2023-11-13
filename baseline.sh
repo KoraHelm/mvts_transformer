252f1554d6917da57a4a5213eb9f6e87c4aa3837
KoraHelm <kora_s.helm@web.de>
2023-11-13 12:09:56 +0100

    adapted bash script

--- baseline.sh
+++ baseline.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 #SBATCH --job-name="recreate_baseline"
 #SBATCH --gres=gpu:1
-#SBATCH --time 0-5:00:00
+#SBATCH --partition=gpu-v100
+#SBATCH --nodes=1
+#SBATCH --time 0-8:00:00
+#SBATCH --ntasks=1
 #SBATCH --output=/mnt/qb/work/eickhoff/esx599/recreate_baseline.out
 #SBATCH --error=/mnt/qb/eickhoff/esx599/recreate_baseline.err
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=kora.helm@student.uni-tuebingen.de
