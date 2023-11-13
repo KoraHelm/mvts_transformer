18302b2d420119cfa9c6b60d157d53e6a75e1e13
KoraHelm <kora_s.helm@web.de>
2023-11-13 12:22:26 +0100

    added run command in bash script

--- baseline.sh
+++ baseline.sh
@@ -9,3 +9,5 @@
 #SBATCH --error=/mnt/qb/eickhoff/esx599/recreate_baseline.err
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=kora.helm@student.uni-tuebingen.de
+
+srun python3 src/main.py --output_dir experiments --comment "classification from Scratch" --name heartbeat --records_file Classification_records.xls --data_dir src/datasets/classification/heartbeat/ --data_class tsra --pattern TRAIN --val_pattern TEST --epochs 400 --lr 0.001 --optimizer RAdam  --pos_encoding learnable  --task classification  --key_metric accuracy
