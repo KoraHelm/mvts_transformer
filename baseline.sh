77a9ee7991d6e6718bc4efce54a0dbc82e28d81a
KoraHelm <kora_s.helm@web.de>
2023-11-14 20:33:02 +0100

    changed gpu

--- baseline.sh
+++ baseline.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name="recreate_baseline"
 #SBATCH --gres=gpu:1
-#SBATCH --partition=gpu-2080ti
+#SBATCH --partition=gpu-v100
 #SBATCH --nodes=1
 #SBATCH --time 0-8:00:00
 #SBATCH --ntasks=1
